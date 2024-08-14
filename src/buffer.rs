7a441aa0d18f3fa24bdf416bb98f2cf56ad1d6d7
Bruce0203 <BruceFreedy@gmail.com>
2024-08-14 17:03:36 +0900

    impl Buf for Box<T: Buf>

--- src/buffer.rs
+++ src/buffer.rs
@@ -128,3 +128,4 @@ impl<T: std::io::Read> ReadToBuf for T {
         Ok(())
     }
 }
+
--- src/traits.rs
+++ src/traits.rs
@@ -1,3 +1,5 @@
+use std::ops::{Deref, DerefMut};
+
 pub trait Buf {
     fn clear(&mut self);
     fn pos(&self) -> usize;
@@ -17,4 +19,58 @@ pub trait ReadBuf: Buf {
     fn advance(&mut self, len: usize);
     fn get_continuous(&self, len: usize) -> &[u8];
     fn remaining(&self) -> usize;
+}
+
+impl<T: Buf> Buf for Box<T> {
+    fn clear(&mut self) {
+        self.deref_mut().clear()
+    }
+
+    fn pos(&self) -> usize {
+        self.deref().pos()
+    }
+
+    fn filled_pos(&self) -> usize {
+        self.deref().filled_pos()
+    }
+
+    unsafe fn set_filled_pos(&mut self, value: usize) {
+        self.deref_mut().set_filled_pos(value)
+    }
+
+    unsafe fn set_pos(&mut self, value: usize) {
+        self.deref_mut().set_pos(value)
+    }
+}
+
+impl<T: ReadBuf> ReadBuf for Box<T> {
+    fn read(&mut self, len: usize) -> &[u8] {
+        self.deref_mut().read(len)
+    }
+
+    fn advance(&mut self, len: usize) {
+        self.deref_mut().advance(len)
+    }
+
+    fn get_continuous(&self, len: usize) -> &[u8] {
+        self.deref().get_continuous(len)
+    }
+
+    fn remaining(&self) -> usize {
+        self.deref().remaining()
+    }
+}
+
+impl<T: WriteBuf> WriteBuf for Box<T> {
+    fn write(&mut self, data: &[u8]) {
+        self.deref_mut().write(data)
+    }
+
+    fn try_write(&mut self, data: &[u8]) -> Result<(), ()> {
+        self.deref_mut().try_write(data)
+    }
+
+    fn remaining_space(&self) -> usize {
+        self.deref().remaining_space()
+    }
 }

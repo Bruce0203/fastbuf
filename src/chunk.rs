dba9d817502d0ed4dba669dee1314743be58a574
Bruce0203 <BruceFreedy@gmail.com>
2024-12-25 13:51:57 +0900

    update

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,9 +1,4 @@
-use core::{
-    alloc::Allocator,
-    mem::{transmute, transmute_copy, MaybeUninit},
-    ops::{Deref, DerefMut},
-    ptr::{slice_from_raw_parts, slice_from_raw_parts_mut},
-};
+use core::{alloc::Allocator, mem::MaybeUninit};
 
 use crate::{declare_impl, declare_trait};
 
@@ -11,8 +6,8 @@ declare_trait! {
     pub trait Chunk<(T, const N: usize, A: Allocator)>: () {
         fn new_uninit_in(alloc: A) -> Self;
         fn new_uninit() -> Self;
-        fn as_slice(&self) -> &[T];
-        fn as_mut_slice(&mut self) -> &mut [T];
+        fn as_slice(&self) -> &[T; N];
+        fn as_mut_slice(&mut self) -> &mut [T; N];
         fn as_ptr(&self) -> *const T;
         fn as_mut_ptr(&mut self) -> *mut T;
     }
@@ -21,73 +16,58 @@ declare_trait! {
 declare_impl! {
     (impl<T, const N: usize, A: Allocator> Chunk<T, N, A> for [T; N]),
     (impl<T, const N: usize, A: Allocator> const Chunk<T, N, A> for [T; N]) {
-        #[inline(always)]
-        fn as_slice(&self) -> &[T] {
-            self
-        }
-
-        #[inline(always)]
-        fn as_mut_slice(&mut self) -> &mut [T] {
-            self
-        }
-
-        #[inline(always)]
-        fn new_uninit_in(alloc: A) -> Self {
-            core::mem::forget(alloc);
-            <[T; N] as Chunk<T, N, A>>::new_uninit()
-        }
-
-        #[inline(always)]
-        fn as_ptr(&self) -> *const T {
-            <[T]>::as_ptr(self)
-        }
-
-        #[inline(always)]
-        fn as_mut_ptr(&mut self) -> *mut T {
-            <[T]>::as_mut_ptr(self)
-        }
-
-        #[inline(always)]
-        fn new_uninit() -> Self {
-            unsafe { MaybeUninit::uninit().assume_init() }
-        }
+    fn as_slice(&self) -> &[T; N] {
+        self
     }
-}
 
-fn asdf() {
-    let a: *const u8 = &0;
+    fn as_mut_slice(&mut self) -> &mut [T; N] {
+        self
+    }
+
+    fn new_uninit_in(alloc: A) -> Self {
+        core::mem::forget(alloc);
+        <[T; N] as Chunk<T, N, A>>::new_uninit()
+    }
+
+    fn as_ptr(&self) -> *const T {
+        <[T]>::as_ptr(self)
+    }
+
+    fn as_mut_ptr(&mut self) -> *mut T {
+        <[T]>::as_mut_ptr(self)
+    }
+
+    fn new_uninit() -> Self {
+        unsafe { MaybeUninit::uninit().assume_init() }
+    }
+
+    }
 }
 
 #[cfg(all(not(feature = "const-trait"), feature = "std"))]
 declare_impl! {
     (impl<T, const N: usize, A: Allocator> Chunk<T, N, A> for Box<[T; N], A>),
     (impl<T, const N: usize, A: Allocator> const Chunk<T, N, A> for Box<[T; N], A>) {
-        #[inline(always)]
-        default fn as_slice(&self) -> &[T] {
-            &**self
+        default fn as_slice(&self) -> &[T; N] {
+            self
         }
 
-        #[inline(always)]
-        default fn as_mut_slice(&mut self) -> &mut [T] {
-            &mut **self
+        default fn as_mut_slice(&mut self) -> &mut [T; N] {
+            self
         }
 
-        #[inline(always)]
         default fn new_uninit_in(alloc: A) -> Self {
             unsafe { Box::new_uninit_in(alloc).assume_init() }
         }
 
-        #[inline(always)]
         default fn as_ptr(&self) -> *const T {
             <[T]>::as_ptr(&**self)
         }
 
-        #[inline(always)]
         default fn as_mut_ptr(&mut self) -> *mut T {
             <[T]>::as_mut_ptr(&mut **self)
         }
 
-        #[inline(always)]
         default fn new_uninit() -> Self {
             unreachable!()
         }
@@ -98,32 +78,26 @@ declare_impl! {
 declare_impl! {
     (impl<T, const N: usize> Chunk<T, N, std::alloc::Global> for Box<[T; N], std::alloc::Global>),
     (impl<T, const N: usize> const Chunk<T, N, std::alloc::Global> for Box<[T; N], std::alloc::Global>) {
-        #[inline(always)]
-        default fn as_slice(&self) -> &[T] {
-            self.deref()
+        default fn as_slice(&self) -> &[T; N] {
+            self
         }
 
-        #[inline(always)]
-        default fn as_mut_slice(&mut self) -> &mut [T] {
-            self.deref_mut()
+        default fn as_mut_slice(&mut self) -> &mut [T; N] {
+            self
         }
 
-        #[inline(always)]
         default fn new_uninit_in(_alloc: std::alloc::Global) -> Self {
             unreachable!()
         }
 
-        #[inline(always)]
         default fn as_ptr(&self) -> *const T {
             <[T]>::as_ptr(&**self)
         }
 
-        #[inline(always)]
         default fn as_mut_ptr(&mut self) -> *mut T {
             <[T]>::as_mut_ptr(&mut **self)
         }
 
-        #[inline(always)]
         default fn new_uninit() -> Self {
             unsafe { Box::new_uninit().assume_init() }
         }
